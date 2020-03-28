64d0d9e02066d706a41f6be7075c79cfd9a400ec
Anass <azeroual.anass2015@gmail.com>
2020-03-28 18:03:38 +0100

    change api

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 var express = require('express');
+var path = require('path');
 var logger = require('morgan');
+var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 var appRoutes = require('./routes/app');
 
@@ -8,19 +10,14 @@ var app = express();
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
-
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
 app.use(function(req, res, next) {
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
   res.setHeader('Access-Control-Allow-Methods', 'POST, GET, PATCH, DELETE, OPTIONS');
   next();
 });
-
-app.use(function(req, res, next) {
-  
-  next();
-});
-
 app.use('/', appRoutes);
 
 module.exports = app;
